afdabb1fae794021ffef4582b47e3cea38cbde5f
Josh Hess <jhesski@gmail.com>
2020-04-23 23:57:19 -0400

    Added Player component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Player from './components/Player/Player';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Player"/>
+        <Player />
       </header>
     </div>
   );
