3f1ab397fc5c65fc16e7d5b6861d9c8962edc47f
Josh Hess <jhesski@gmail.com>
2020-04-24 00:58:43 -0400

    added background container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Player from './components/Player/Player';
+import View from './components/View/View';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <View>
         <Player />
+      </View>  
       </header>
     </div>
   );
